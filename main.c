1b5d2a5271c6779db7f05b634b81ef7d3decb58f
ChristophGreger <mail@christoph-greger.de>
2024-06-15 12:51:29 +0200

    more stuff

--- main.c
+++ main.c
@@ -2,4 +2,10 @@
 // Created by Christoph Greger on 15.06.24.
 //
 
+#include <stdio.h>
 #include "main.h"
+
+int main(int argc, char *argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
--- main.h
+++ main.h
@@ -2,7 +2,10 @@
 // Created by Christoph Greger on 15.06.24.
 //
 
+
 #ifndef THEORETICALINTERPRETER_MAIN_H
 #define THEORETICALINTERPRETER_MAIN_H
 
 #endif //THEORETICALINTERPRETER_MAIN_H
+
+int main(int argc, char *argv[]);
