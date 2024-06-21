ba99696f3e4fd0c6d55effb25050957e130cdf3f
ChristophGreger <mail@christoph-greger.de>
2024-06-21 20:35:39 +0200

    added parser. Not tested yet

--- Parser/parser.c
+++ Parser/parser.c
@@ -3,5 +3,52 @@
 //
 
 #include "parser.h"
+#include <stdlib.h>
 
 
+
+
+struct ASTNodeHelper *parseHelp(TokenStruct *firstToken) {
+    struct ASTNodeHelper *firstNode = malloc(sizeof(struct ASTNodeHelper));
+    struct ASTNodeHelper *currentNode = firstNode;
+    TokenStruct *currentToken = firstToken;
+    while (currentToken->token != EOFT) {
+        switch (currentToken->token) {
+            case Loop:
+                currentNode->node->type = LOOP;
+                currentNode->node->value.loop.x = *((int *) currentToken->value);
+                currentToken = currentToken->next;
+                struct ASTNodeHelper *body = parseHelp(currentToken);
+                currentNode->node->value.loop.body = body->node;
+                currentToken = body->nextToken;
+                break;
+            case Increment:
+                currentNode->type = INCREMENT;
+                currentNode->value.increment = *((int *) currentToken->value);
+                currentToken = currentToken->next;
+                break;
+            case Reset:
+                currentNode->type = RESET;
+                currentNode->value.reset = *((int *) currentToken->value);
+                currentToken = currentToken->next;
+                break;
+            case closecurly:
+                firstNode->nextToken = currentToken->next;
+                return firstNode;
+            default:
+                exit(1);
+        }
+        if (currentToken != NULL) {
+            currentNode->node->next = malloc(sizeof(struct ASTNode));
+            currentNode = currentNode->node->next;
+        }
+    }
+    return firstNode;
+
+}
+
+
+struct ASTNode *parse(TokenStruct *firstToken) {
+    struct ASTNodeHelper *helper = parseHelp(firstToken);
+    return helper->node;
+}
--- Parser/parser.h
+++ Parser/parser.h
@@ -7,7 +7,30 @@
 
 #include "../lexical/lexicalanalyzer.h"
 
-struct AST
+typedef struct ASTNodeHelper {
+    int error; //0 wenn kein Fehler, 1 wenn Fehler
+    TokenStruct *nextToken;
+    struct ASTNode *node;
+};
+
+typedef struct ASTNode {
+    enum { //enum für die verschiedenen Typen von Knoten
+        LOOP,
+        INCREMENT,
+        RESET,
+    } type;
+    union {
+        struct { //struct für Loop
+            int x;
+            struct ASTNode *body; //Pointer auf den Body des Loops
+        } loop;
+        int increment; //int für Increment
+        int reset; //int für Reset
+    } value;
+    struct ASTNode *next; //Pointer auf den nächsten Knoten
+};
+
+struct ASTNode *parse(TokenStruct *firstToken);
 
 
 #endif //THEORETICALINTERPRETER_PARSER_H
